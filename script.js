d4346c1c40a044537acc72d3061762fd520740c3
Muzzamil Bukhari <muhammadmuzzamilbukhari@gmail.com>
2024-02-20 08:24:39 +0500

    date functionality added

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-let todayDate = new Date();
 
 const displayMsg = (years, months, days) => {
     let msg = document.querySelector(".msg");
@@ -6,19 +5,36 @@ const displayMsg = (years, months, days) => {
 }
 
 const calculateAge = () => {
+    let todayDate = new Date ();
+    let currYear = todayDate.getFullYear();
+    let currMonth = todayDate.getMonth();
+    let currDay = todayDate.getDate();
     let dobValue = document.querySelector("input").value;
     let birthDateInArray = dobValue.split("-");
-    let yearsDiff = todayDate.getFullYear() - birthDateInArray[0];
-    let monthsDiff = todayDate.getMonth()+1 - birthDateInArray[1];
+    let birthYear = birthDateInArray[0];
+    let birthMonth = birthDateInArray[1];
+    let birthDate = birthDateInArray[2];
+
+    let yearsDiff, monthsDiff, daysDiff;
+    yearsDiff = currYear - birthYear;
+    monthsDiff = currMonth + 1 - birthMonth;
+    daysDiff = currDay - birthDate;
+    if (monthsDiff < 0){
+        yearsDiff--;
+        monthsDiff += 12;
+    }
+    if (daysDiff < 0){
+        monthsDiff--;
+        daysDiff += getDaysInMonth(birthYear, birthMonth);
+    }
     if (monthsDiff < 0){
         monthsDiff += 12;
         yearsDiff--;
     }
-    let daysDiff = todayDate.getDate() - birthDateInArray[2];
-    if (daysDiff < 0){
-        daysDiff += 30;
-        monthsDiff--;
-    }
     displayMsg(yearsDiff, monthsDiff, daysDiff);
+}
+
+const getDaysInMonth = (year, month) => {
+    return new Date (year, month, 0).getDate();
 }
 // console.log(date);
